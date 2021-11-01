547ede0485223f4060b0a993b11e9cecfa354fbe
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-11-02 00:36:28 +0530

    Added:Find maximum value using generic

---
+++ FindMaximumUsingGenric/FindMaximumUsingGenric/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace FindMaximumUsingGenric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("maximum integer number");
+           
+            int maxIntNumber = MaximumNumberCheck.maximumIntegerNumber(50, 150, 600);
+            Console.WriteLine(maxIntNumber);
+
+           Console.WriteLine("Maximum float value ");
+            double maxFloateNumber = UC2FloatNumberMax.maximumFloateNumber(4.5, 9.5, 2.1);
+            Console.WriteLine(maxFloateNumber);
+
+            Console.WriteLine("Maximum string name ");
+            string maxString = UC3MaxString.MaximumString("Apple", "Peach", "Banana");
+            Console.WriteLine(maxString);
+
+
+            Console.WriteLine("Generic value in use case 4 ");
+            int maxNumber = UC4GenericMaxNumber.findMaximum<int>(100, 300,500);
+            double maxdoubleNumber = UC4GenericMaxNumber.findMaximum<double>(5.6, 3.5, 5.9);
+            string maxStringName = UC4GenericMaxNumber.findMaximum<string>("Apple", "Peach","Banana");
+            Console.WriteLine(maxNumber);
+            Console.WriteLine(maxdoubleNumber);
+            Console.WriteLine(maxStringName);
+     
+
+            
+            
+
+            /*Console.WriteLine("Generic value in use case 5 ");
+            int[] arr = { 123, 160, 450, 60 };
+            UC5PrintMaxMethodToStdOut<int> generic = new UC5PrintMaxMethodToStdOut<int>(arr);
+            generic.printMaxValue();
+*/
+
+
+
+
+        }
+    }
+
+    
+
+        
+    }
+
---
+++ FindMaximumUsingGenric/FindMaximumUsingGenric/UC4GenericMaxNumber.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMaximumUsingGenric
+{
+    public   class UC4GenericMaxNumber
+    {
+        /*public T firstValue, secondValue, thirdValue;
+        private int[] arr;
+
+        public UC4GenericMaxNumber(T firstValue, T secondValue, T thirdValue)
+        {
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+            this.thirdValue = thirdValue;
+        }
+
+        public UC4GenericMaxNumber(int[] arr)
+        {
+            this.arr = arr;
+        }
+*/
+        public static T findMaximum<T>(T firstValue, T secondValue, T thirdValue) where T : IComparable
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) > 0 ||
+                secondValue.CompareTo(thirdValue) >= 0 && secondValue.CompareTo(firstValue) > 0 ||
+                secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) >= 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) > 0 ||
+                thirdValue.CompareTo(secondValue) >= 0 && thirdValue.CompareTo(firstValue) > 0 ||
+                thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) >= 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstValue,secondValue,thirdValue are same");
+        }
+       
+
+
+
+
+
+
+
+
+        /* public T maxMethod()
+          {
+              T max = UC4GenericMaxNumber<T>.TestMaximum(this.firstValue, this.secondValue, this.thirdValue);
+              return max;
+          }
+  
+        public T TestMaximum(T firstValue, T secondValue, T thirdValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static T maxValue(params T[] values)
+        {
+            T[] sortedValues = Sort(values);
+            return sortedValues[^1];
+        }
+
+
+        public static T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }*/
+    }
+}
